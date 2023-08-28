2ab92dd00f250314ded8ef87fd2798442ffe6a50
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-28 13:49:54 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from '../SharedLayout/SharedLayout';
+import Home from 'pages/Home/Home';
+import Movies from 'pages/Movies/Movies';
+import MovieDetails from 'pages/MovieDetails/MovieDetails';
+import { Cast } from 'components/Cast/Cast';
+import { Reviews } from 'components/Reviews/Reviews';
+
 export const App = () => {
-  return <div>React homework</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
 };
