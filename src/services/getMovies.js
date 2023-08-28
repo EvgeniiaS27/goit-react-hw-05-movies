de1e15431ab43b56ba919e142ddc84379f744b39
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-28 13:56:47 +0300

    Create getMovies.js

---
+++ src/services/getMovies.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'f8a6c1c08257b05a7c4692b093cbf34e';
+
+export const getTrendingMovies = () => {
+  return fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
+};
+
+export const getMovieDetailes = movieId => {
+  return fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+};
+
+export const getMovieCast = movieId => {
+  return fetch(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`);
+};
+
+export const getMovieReviews = movieId => {
+  return fetch(`${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}`);
+};
+
+export const getSearchMovies = query => {
+  return fetch(`${BASE_URL}/search/movie?query=${query}&api_key=${API_KEY}`);
+};
