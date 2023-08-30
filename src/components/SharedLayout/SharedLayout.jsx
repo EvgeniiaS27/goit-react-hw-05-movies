c7d18749045d4ced30b45487493688e02b210e84
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-30 15:21:21 +0300

    Update component SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,36 @@
+import { Suspense } from 'react';
+import css from './SharedLayout.module.css';
 import { NavLink, Outlet } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
 
 export const SharedLayout = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <NavLink to="/">HOME</NavLink>
-          <NavLink to="/movies">MOVIES</NavLink>
+      <header className={css.header}>
+        <nav className={css.linkList}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${css.active}` : `${css.linkItem}`
+            }
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? `${css.active}` : `${css.linkItem}`
+            }
+            to="/movies"
+          >
+            MOVIES
+          </NavLink>
         </nav>
       </header>
-      <Outlet />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
     </div>
   );
 };
