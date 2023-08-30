77ccf5a4dcd2af6a1267f6090d57b5889f2466e0
EvgeniiaS27 <adm.pr.salionova@gmail.com>
2023-08-30 15:20:01 +0300

    Update component MoviesList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,12 +1,21 @@
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import css from './MoviesList.module.css';
 
 export const MoviesList = ({ movies }) => {
+  const location = useLocation();
+
   return (
-    <ul>
+    <ul className={css.list}>
       {movies.map(({ title, id }) => {
         return (
-          <li key={id}>
-            <NavLink to={`movies/${id}`}>{title}</NavLink>
+          <li key={id} className={css.item}>
+            <Link
+              className={css.itemLink}
+              to={`/movies/${id}`}
+              state={{ from: location }}
+            >
+              {title}
+            </Link>
           </li>
         );
       })}
